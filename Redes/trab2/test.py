9c7a95f72bd1a54672ae06037b178475e5a774de
Bruno Vila Lobus Strapasson <48770007+BrunoVLStrapasson@users.noreply.github.com>
2024-12-01 22:45:35 -0300

    Delete Redes/trab2 directory

--- Redes/trab2/test.py
+++
@@ -1,8 +0,0 @@
-from game import Game
-
-game = Game()
-game.initialize_deck()
-print(f"Printando deck: {game.state['deck']}")
-game.shuffle_deck()
-print(f"Printando deck: {game.state['deck']}")
-print(f"Printando cartas: {game.draw_cards()}")
